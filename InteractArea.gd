36a0e190b303602078746f4e369eac25422f4e65
claramckinley <claramckinley@me.com>
2023-07-24 12:18:27 -0500

    mroe change to the mainmap

---
+++ InteractArea.gd
@@ -0,0 +1,6 @@
+class_name Interactable extends Area2D
+
+export var interactLabel = "none"
+export var interactType = "none"
+export var interactValue = "none"
+
--- Player/Player.gd
+++ Player/Player.gd
@@ -7,12 +7,20 @@ var MAX_SPEED = 80
 var velocity = Vector2.ZERO
 
 onready var animatedSprite = $AnimatedSprite
+onready var allInteractions = []
+onready var interactLabel = $InteractionComponents/InteractLabel
+
+func _ready():
+	update_interactions()
 
 func _physics_process(delta):
 	var inputVector = Vector2.ZERO
 	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	inputVector = inputVector.normalized()
+	
+	if Input.is_action_just_pressed("interact"):
+		execute_interaction()
 	
 	if inputVector != Vector2.ZERO:
 		velocity = velocity.move_toward(inputVector * MAX_SPEED, ACCELERATION * delta)
@@ -30,3 +38,28 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	velocity = move_and_slide(velocity)
 	
+	#Interaction Methods
+	
+func _on_InteractionArea_area_entered(area):
+	allInteractions.insert(0, area)
+	update_interactions()
+
+func _on_InteractionArea_area_exited(area):
+	allInteractions.erase(area)
+	update_interactions()
+	
+func update_interactions():
+	if allInteractions:
+		print()
+		#interactLabel.text = allInteractions[0].interactLabel
+	else:
+		interactLabel.text = ""
+		
+func execute_interaction():
+	if allInteractions:
+		var curInteraction = allInteractions[0]
+		match curInteraction.interactType:
+			"print_text" : print(curInteraction.interactValue)
+			"display_value" : interactLabel.text = curInteraction.interactValue
+			
+
