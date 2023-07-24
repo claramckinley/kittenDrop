23f0c05e762b039f00732d1834860519d4c52ba8
claramckinley <claramckinley@me.com>
2023-07-23 19:31:24 -0500

    started on mainmap

---
+++ Player/Player.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+var FRICTION = 80
+var ACCELERATION = 80
+var MAX_SPEED = 100
+
+var velocity = Vector2.ZERO
+
+onready var animatedSprite = $
+
+
+func _physics_process(delta):
+	var inputVector = Vector2.ZERO
+	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	inputVector = inputVector.normalized()
+	
+	if inputVector != Vector2.ZERO:
+		if()
+		velocity = velocity.move_toward(inputVector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	velocity = move_and_slide(velocity)
