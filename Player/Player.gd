527c7117523d96313da6906316029a88f52cb9f4
claramckinley <claramckinley@me.com>
2023-07-23 20:40:46 -0500

    day one

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,13 +1,12 @@
 extends KinematicBody2D
 
-var FRICTION = 80
-var ACCELERATION = 80
-var MAX_SPEED = 100
+var FRICTION = 500
+var ACCELERATION = 500
+var MAX_SPEED = 80
 
 var velocity = Vector2.ZERO
 
 onready var animatedSprite = $AnimatedSprite
-
 
 func _physics_process(delta):
 	var inputVector = Vector2.ZERO
@@ -17,6 +16,17 @@ func _physics_process(delta):
 	
 	if inputVector != Vector2.ZERO:
 		velocity = velocity.move_toward(inputVector * MAX_SPEED, ACCELERATION * delta)
+		if velocity.x > 0:
+			animatedSprite.play("WalkRight")
+		elif velocity.x < 0:
+			animatedSprite.play("WalkLeft")
+		elif velocity.y > 0:
+			animatedSprite.play("WalkDown")
+		else:
+			animatedSprite.play("WalkUp")
+		
 	else:
+		animatedSprite.play("IdleDown")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	velocity = move_and_slide(velocity)
+	
